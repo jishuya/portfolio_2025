6bca04a1569c91192475419522d17d0600a739cb
jishuya <jishuya3015@gmail.com>
2025-06-26 15:43:27 +0900

    JS - Refactoring

--- src/projects.js
+++ src/projects.js
@@ -1,33 +1,35 @@
 'use strict';
 
+// 프로젝트 필터링 관련 로직 처리
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
 const projectsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  // 현재 categories에 이벤트가 걸려있기때문에 빈 공간 클릭하면 dataset =null이 나옴
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
 
-categories.addEventListener('click', (event)=>{
-    const filter = event.target.dataset.category;
-    if (filter === null) {
-        return;
-    } 
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
 
-    // Active 메뉴를 재설정
-    const active = document.querySelector('.category--selected');
-    active.classList.remove('category--selected'); //classList쓸 때는 . # 이런거 꼭 빼야함
-    event.target.classList.add('category--selected');
-
-    
-
-    // 프로젝트 필터링
-    projectsContainer.classList.add('anim-out');
-    projects.forEach((project)=>{
-        if(filter === 'all' || filter === project.dataset.type) {
-            project.style.display = 'block';
-        } else {
-            project.style.display = 'none';
-        }
-    })
-
-    setTimeout(()=>{
-      projectsContainer.classList.remove('anim-out');
-    }, 250)
-})
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
